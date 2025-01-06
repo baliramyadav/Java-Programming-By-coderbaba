6d4bc0fd0ca18a4f2e2dbfce274817654dd85660
Baliram Yadav <ballubalrambca@gmail.com>
2025-01-06 19:56:26 +0530

    patter2

---
+++ JavaCourseProgram/src/Day2/Javaprogram/StarPattern3.java
@@ -0,0 +1,41 @@
+package Day2.Javaprogram;
+import java.util.Scanner;
+public class StarPattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in); //Input
+
+		System.out.println("Enter the number of rows: ");
+
+		int rows = sc.nextInt();
+
+		for (int i = 0; i <= rows - 1; i++) { //For Loop for Row 
+
+		for (int j = 0; j <= i; j++) { //For Loop for Col
+
+		System.out.print("*" + " "); //prints * and blank space
+
+		}
+
+		System.out.println(""); // new line
+
+		}
+
+		for (int i = rows - 1; i >= 0; i--) { //For Loop for Row
+
+		for (int j = 0; j <= i - 1; j++) { //For Loop for Col
+
+		System.out.print("*" + " "); //prints * and blank space
+
+		}
+
+		System.out.println("");// new line
+
+		}
+
+		sc.close();
+
+	}
+
+}
---
+++ JavaCourseProgram/src/Day2/Javaprogram/StarPattern4.java
@@ -0,0 +1,36 @@
+package Day2.Javaprogram;
+
+public class StarPattern4 {
+	public static void printStars(int n) {
+
+		int i, j;
+
+		for (i = 0; i < n; i++) {  
+
+		for (j = 2 * (n - i); j >= 0; j--) { //For Loop for Row
+
+		System.out.print(" "); // Print Spaces
+
+		}
+
+		for (j = 0; j <= i; j++) { //For Loop for col
+
+		System.out.print("* "); // Print Star
+
+		}
+
+		System.out.println();
+
+		}
+
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n = 5; //Number of Rows
+
+		printStars(n);
+	}
+
+}
