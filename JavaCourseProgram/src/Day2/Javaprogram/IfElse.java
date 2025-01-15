8f59a893d8978bfcfd3aa0eb9869fb9091fab50f
Baliram Yadav <ballubalrambca@gmail.com>
2025-01-15 19:34:59 +0530

    15Jann2025

---
+++ JavaCourseProgram/src/Day2/Javaprogram/IfElse.java
@@ -0,0 +1,16 @@
+package Day2.Javaprogram;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10;
+
+        if (n > 5) {
+            System.out.println("The number is greater than 5.");
+        } else {
+            System.out.println("The number is 5 or less.");
+        }
+	}
+
+}
---
+++ JavaCourseProgram/src/Day2/Javaprogram/IfElseExample.java
@@ -0,0 +1,23 @@
+package Day2.Javaprogram;
+
+public class IfElseExample {
+
+	public static void main(String[] args) {
+		// if else statement
+		boolean a = true;
+        boolean b = false;
+        
+        if (a) {
+            System.out.println("a is true");
+        } else {
+            System.out.println("a is false");
+        }
+        
+        if (b) {
+            System.out.println("b is true");
+        } else {
+            System.out.println("b is false");
+        }
+	}
+
+}
