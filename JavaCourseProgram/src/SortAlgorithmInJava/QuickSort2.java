0048d6881b766162fda6b0e7916811fa35fe4a40
Baliram Yadav <ballubalrambca@gmail.com>
2025-01-17 19:50:41 +0530

    sorting3

---
+++ JavaCourseProgram/src/SortAlgorithmInJava/QuickSort2.java
@@ -0,0 +1,11 @@
+package SortAlgorithmInJava;
+ 
+ 
+public class QuickSort2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter  elements ");
+	}
+
+}
