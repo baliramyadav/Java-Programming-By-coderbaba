af36b518da8ba90841f8feae997f397a85ed8e9c
Baliram Yadav <ballubalrambca@gmail.com>
2025-02-09 19:49:14 +0530

    7Feb2025

---
+++ JavaCourseProgram/src/Day1/OddNumber.java
@@ -0,0 +1,18 @@
+package Day1;
+
+public class OddNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int num = 5;
+
+		 if(num % 2 == 0)
+
+		 System.out.println(num + " is even.");
+
+		 else
+
+		 System.out.println(num + " is odd.");
+	}
+
+}
---
+++ JavaCourseProgram/src/Day1/TwoNumbers.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class TwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 5, num2 = 10, sum;
+
+		 sum = num1 + num2;
+
+		 System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+	}
+
+}
